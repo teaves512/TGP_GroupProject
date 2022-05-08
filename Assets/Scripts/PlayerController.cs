342fc8eb1113c650e0266c7ac22f4a1bdb5216c4
Tommy Eaves <teaves512@users.noreply.github.com>
2022-05-08 18:27:14 +0100

    Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,38 +12,83 @@ public class PlayerController : MonoBehaviour
         RIGHT
     };
 
-    private Direction m_Direction;
+    private int m_GridPosX = 0;
+    private int m_GridPosZ = 0;
+    [SerializeField] private float m_GridCellSize = 1.0f;
+    [SerializeField] private float m_GridMovementAllowance = 0.01f;
 
-    private Coroutine m_cMove;
+    [SerializeField] private float m_MoveSpeed = 10.0f;
+
+    private Direction m_Direction = Direction.UP;
+
+    private Coroutine m_cMove = null;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        m_GridPosX = 0;
+        m_GridPosZ = 0;
+
+        m_Direction = Direction.UP;
+
+        m_cMove = null;
+    }
 
     private void Update()
     {
         //check for directional input
-        if (Input.GetKeyDown(KeyCode.W) ||
-            Input.GetKeyDown(KeyCode.S) ||
-            Input.GetKeyDown(KeyCode.A) ||
-            Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.W) ||
+            Input.GetKey(KeyCode.S) ||
+            Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.D))
         {
-            if (Input.GetKeyDown(KeyCode.W))
+            //determine the direction the player wants to move
+            if (Input.GetKey(KeyCode.W))
             { m_Direction = Direction.UP; }
-            else if (Input.GetKeyDown(KeyCode.S))
+            else if (Input.GetKey(KeyCode.S))
             { m_Direction = Direction.DOWN; }
-            else if (Input.GetKeyDown(KeyCode.A))
+            else if (Input.GetKey(KeyCode.A))
             { m_Direction = Direction.LEFT; }
-            else if (Input.GetKeyDown(KeyCode.D))
+            else if (Input.GetKey(KeyCode.D))
             { m_Direction = Direction.RIGHT; }
 
-
+            //start moving, if not already moving
             if (m_cMove == null)
-            {
-                //start moving in direction
-            }
+            { Move(); }
         }
+    }
+
+    private void Move()
+    {
+        //ensure there are no existing instances of the C_Move coroutine
+        if (m_cMove != null) { StopCoroutine(m_cMove); }
+        //assign a new instance of the C_Move coroutine
+        m_cMove = StartCoroutine(C_Move());
     }
 
     private IEnumerator C_Move()
     {
-        yield return new WaitForEndOfFrame();
+        //adjust the grid cell position, appropriately
+        if (m_Direction == Direction.UP)            { m_GridPosZ++; }
+        else if (m_Direction == Direction.DOWN)     { m_GridPosZ--; }
+        else if (m_Direction == Direction.LEFT)     { m_GridPosX--; }
+        else if (m_Direction == Direction.RIGHT)    { m_GridPosX++; }
+
+        //determine the target position, in world space
+        Vector3 targetPosition = new Vector3(m_GridPosX, 0, m_GridPosZ) * m_GridCellSize;
+
+        while (Vector3.SqrMagnitude(transform.position - targetPosition) > m_GridMovementAllowance)
+        {
+            Vector3 direction = (targetPosition - transform.position).normalized;
+
+            transform.Translate(direction * m_MoveSpeed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+        transform.position = targetPosition;
 
         m_cMove = null;
     }
