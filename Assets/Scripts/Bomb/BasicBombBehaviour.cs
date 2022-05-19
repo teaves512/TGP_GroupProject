fe3818c4fbd706dda2962aac03df094a7aa31c24
WillBennett2 <72083740+WillBennett2@users.noreply.github.com>
2022-05-19 15:27:30 +0100

    basic bomb placement fix
    + shifting z back

--- Assets/Scripts/Bomb/BasicBombBehaviour.cs
+++ Assets/Scripts/Bomb/BasicBombBehaviour.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class BasicBombBehaviour : MainBombBehaviour
 {
+	[SerializeField]private float m_MoveBack;
     protected override void Update()
     {
-        if (Input.GetKeyDown("l"))
+        if (Input.GetKeyDown("l")&&!m_Exploded)
         {
             StartCoroutine(Explode());
         }
@@ -21,16 +22,18 @@ public class BasicBombBehaviour : MainBombBehaviour
     protected override IEnumerator Explode()
     {
         m_Exploded = true;
-        Debug.Log("boom");
+
         //check if this is destructable
         RaycastHit hit;
-        Debug.DrawRay(transform.position, transform.forward, Color.red);
-        if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity))
+        if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z+m_MoveBack), transform.forward, out hit))
         {
             Debug.DrawRay(hit.transform.position, transform.forward, Color.red);
             yield return new WaitForSeconds(m_DestroyDelay);
-            if(hit.collider.gameObject.layer==m_LayerMask)
-                hit.collider.gameObject.GetComponent<Destructable>().TakeDamage(m_Damage);
+			Debug.Log(hit.collider.gameObject.layer);
+			if (hit.collider.gameObject.layer == 6)
+			{
+				hit.collider.gameObject.GetComponent<Destructable>().TakeDamage(m_Damage);
+			}
             hit.collider.tag = "Placeable";
 
         }
