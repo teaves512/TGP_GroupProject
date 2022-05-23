6ace204f3b5200654b7c37b888ed45094c6b7ff3
Adeogo Adeniji <68272077+Adeogo1@users.noreply.github.com>
2022-05-23 16:07:58 +0100

    gameover + health

---
+++ Assets/GameOverButton.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverButton : ChangeSceneButton
+{
+    public enum buttontype
+    {
+        CONTINUE,
+        RETRY,
+        EXIT
+    }
+
+    public GameObject m_ContinueButton;
+    protected override void Awake()
+    {
+        if (m_ContinueButton.activeInHierarchy)
+        {
+            m_ContinueButton.gameObject.SetActive(false);
+        }
+
+        EventManager.GameOver += GameOver;
+        base.Awake();
+    }
+
+
+    [SerializeField] private buttontype M_GameOverScreenButton;
+    protected override void ButtonAction()
+    {
+        switch (M_GameOverScreenButton)
+        {
+            case buttontype.CONTINUE:
+                ContinueButtonFunction();
+                break;
+            case buttontype.RETRY:
+                RetryButtonFunction();
+                break;
+            case buttontype.EXIT:
+                ExitButtonFunction();
+                break;
+            default:
+                break;
+        }
+        base.ButtonAction();
+        
+    }
+
+    void GameOver(bool victory)
+    {
+        if (victory && !m_ContinueButton.activeInHierarchy)
+        {
+            m_ContinueButton.gameObject.SetActive(true);
+        }
+    }
+
+    void ContinueButtonFunction()
+    {
+        
+    }
+    
+    void RetryButtonFunction()
+    {
+        
+    }
+    
+    void ExitButtonFunction()
+    {
+        
+    }
+    
+}
