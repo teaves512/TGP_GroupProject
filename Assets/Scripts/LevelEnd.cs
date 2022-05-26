e55383fe8717e7ad1d57992b98469eef596aeb67
Jade-Marker <m012677j@student.staffs.ac.uk>
2022-05-26 17:07:36 +0100

    Level end fade out

--- Assets/Scripts/LevelEnd.cs
+++ Assets/Scripts/LevelEnd.cs
@@ -1,13 +1,41 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelEnd : MonoBehaviour
 {
     [SerializeField] string menuScene;
+    [SerializeField] float m_FadeTime;
+    [SerializeField] Image m_Image;
+
+    Coroutine m_Coroutine;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
-            SceneManager.LoadScene(menuScene, LoadSceneMode.Single);
+        {
+            if (m_Coroutine == null)
+                m_Coroutine = StartCoroutine(EndLevel());
+        }
+            
+    }
+
+    private IEnumerator EndLevel()
+    {
+        float startTime = Time.time;
+        float currentTime = Time.time - startTime;
+        while(currentTime < m_FadeTime)
+        {
+            currentTime = Time.time - startTime;
+
+            float t = currentTime / m_FadeTime;
+
+            m_Image.color = new Color(m_Image.color.r, m_Image.color.g, m_Image.color.b, t);
+
+            yield return null;
+        }
+
+        SceneManager.LoadScene(menuScene, LoadSceneMode.Single);
     }
 }
