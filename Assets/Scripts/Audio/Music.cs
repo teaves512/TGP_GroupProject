81cc298f842707a7e11cd8696b7b5aceaa081dbb
Declan Shields <69156727+declanshields@users.noreply.github.com>
2022-05-26 16:23:05 +0100

    added music script and end zones in each level

---
+++ Assets/Scripts/Audio/Music.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Music : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch(SceneManager.GetActiveScene().name)
+        {
+            case "Tutorial2":
+                AudioManager.SetMusic("Tutorial");
+                break;
+            case "Level Mockup":
+                AudioManager.SetMusic("Level 1");
+                break;
+            case "Level 2 Mock":
+                AudioManager.SetMusic("Level 2");
+                break;
+            case "Level 3 Mock":
+                AudioManager.SetMusic("Level 3");
+                break;
+            case "Level 5":
+                AudioManager.SetMusic("Level 4");
+                break;
+            case "TomL1":
+                AudioManager.SetMusic("Level 5");
+                break;
+            case "Boss Fight":
+                AudioManager.SetMusic("Boss");
+                break;
+        }
+        AudioManager.PlayMusic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        AudioManager.PlayMusic(false);
+    }
+}
