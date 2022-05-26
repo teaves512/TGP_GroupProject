9728ec71af5a2fbe990b5786112dc24ae2fb379a
Declan Shields <69156727+declanshields@users.noreply.github.com>
2022-05-26 14:37:03 +0100

    Update UIPositionScale.cs

--- Assets/Scripts/UI/UIPositionScale.cs
+++ Assets/Scripts/UI/UIPositionScale.cs
@@ -10,6 +10,7 @@ public class UIPositionScale : MonoBehaviour
     [SerializeField] Vector2 m_originalPosition;
     [SerializeField] ScalingMode m_ScalingMode;
 
+    Vector2 m_ScreenResolution;
     RectTransform m_rectTransform;
     RectTransform m_canvasRectTransform;
 
@@ -24,14 +25,23 @@ public class UIPositionScale : MonoBehaviour
         m_canvasRectTransform = m_canvasScaler.GetComponent<RectTransform>();
         m_rectTransform = GetComponent<RectTransform>();
 
-        MoveUIForNewResolution(m_canvasRectTransform.sizeDelta);
+        OnResolutionChange(m_canvasRectTransform.sizeDelta);
     }
 
-    void MoveUIForNewResolution(Vector2 newResolution)
+    void Update()
     {
+        if (m_canvasRectTransform.sizeDelta != m_ScreenResolution)    //Needed in update, since Unity doesn't have a resolution change callback
+            OnResolutionChange(m_canvasRectTransform.sizeDelta);
+    }
+
+    void OnResolutionChange(Vector2 newResolution)
+    {
+        m_ScreenResolution = newResolution;
+
         if(m_ScalingMode == ScalingMode.WIDTH)
             m_rectTransform.anchoredPosition = new Vector2(m_originalPosition.x * (m_canvasRectTransform.rect.width / m_canvasScaler.referenceResolution.x), m_rectTransform.anchoredPosition.y);
         else
             m_rectTransform.anchoredPosition = new Vector2(m_rectTransform.anchoredPosition.x, m_originalPosition.y * (m_canvasRectTransform.rect.height / m_canvasScaler.referenceResolution.y));
+
     }
 }
